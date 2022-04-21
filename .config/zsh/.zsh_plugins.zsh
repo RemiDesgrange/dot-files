394c2db66b7583978d8e16138177dd9d0e30eacc
Remi Desgrange <remidesgrange@ielo.net>
2022-04-21 17:06:53 +0200

    bump config

--- .config/zsh/.zsh_plugins.zsh
+++ .config/zsh/.zsh_plugins.zsh
@@ -1,4 +1,6 @@
-fpath+=( /Users/remi/.config/zsh/.zephyr/plugins/zshfish )
-source /Users/remi/.config/zsh/.zephyr/plugins/zshfish/zshfish.zsh-theme
-fpath+=( /Users/remi/.config/zsh/.zephyr/plugins/ssh )
-source /Users/remi/.config/zsh/.zephyr/plugins/ssh/ssh.plugin.zsh
+fpath+=( /Users/remi/.config/zsh/.zephyr/plugins/Raniconduh__zshfish )
+source /Users/remi/.config/zsh/.zephyr/plugins/Raniconduh__zshfish/zshfish.zsh-theme
+fpath+=( /Users/remi/.config/zsh/.zephyr/plugins/zpm-zsh__ssh )
+source /Users/remi/.config/zsh/.zephyr/plugins/zpm-zsh__ssh/ssh.plugin.zsh
+fpath+=( /Users/remi/.config/zsh/.zephyr/plugins/romkatv__zsh-prompt-benchmark )
+source /Users/remi/.config/zsh/.zephyr/plugins/romkatv__zsh-prompt-benchmark/zsh-prompt-benchmark.plugin.zsh
