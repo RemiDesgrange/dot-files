9bcc13bdfb62fb4720845fbd54b6373cc4348bf4
Remi Desgrange <remidesgrange@ielo.net>
2022-04-22 22:48:57 +0200

    remove antidote and zephyr

--- .config/zsh/.zsh_plugins.zsh
+++
@@ -1,6 +0,0 @@
-fpath+=( /Users/remi/.config/zsh/.zephyr/plugins/Raniconduh__zshfish )
-source /Users/remi/.config/zsh/.zephyr/plugins/Raniconduh__zshfish/zshfish.zsh-theme
-fpath+=( /Users/remi/.config/zsh/.zephyr/plugins/zpm-zsh__ssh )
-source /Users/remi/.config/zsh/.zephyr/plugins/zpm-zsh__ssh/ssh.plugin.zsh
-fpath+=( /Users/remi/.config/zsh/.zephyr/plugins/romkatv__zsh-prompt-benchmark )
-source /Users/remi/.config/zsh/.zephyr/plugins/romkatv__zsh-prompt-benchmark/zsh-prompt-benchmark.plugin.zsh
